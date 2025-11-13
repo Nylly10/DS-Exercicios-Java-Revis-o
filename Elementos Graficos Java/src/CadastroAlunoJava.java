b7b818ad4dba3e764e54548fb447ca52209d5eaa
Nylly Anny <nylly.yaya@gmail.com>
2025-11-13 12:49:54 -0300

    Add files via upload

---
+++ Elementos Graficos Java/src/CadastroAlunoJava.java
@@ -0,0 +1,81 @@
+//Classe modelo
+class Aluno{
+
+    private int id;
+    private String nome;
+    private String email;
+    private String curso;
+    private String genero;
+    private boolean receberEmail;
+    private boolean receberNotificacao;
+    private String rua;
+    private String cidade;
+
+    //Getter e Setters
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String text) {
+        this.email = email;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+    public void setCurso(String selectedItem) {
+        this.curso = curso;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+    public void setGenero() {
+        this.genero = genero;
+    }
+
+    public boolean isReceberEmail() {
+        return receberEmail;
+    }
+    public void setReceberEmail(boolean receberEmail) {
+        this.receberEmail = receberEmail;
+    }
+
+    public  boolean isReceberNotificacao() {
+        return receberNotificacao;
+    }
+    public void setReceberNotificacao(boolean receberNotificacao) {
+        this.receberNotificacao = receberNotificacao;
+    }
+
+    public  String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setGenero(String s) {
+    }
+}
